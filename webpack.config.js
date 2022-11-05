443f9cacc8c1163262b5d3506e1891101de7f5bd
wetzka <matthias.wetzka@gmx.de>
2022-11-05 15:44:58 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/main.js',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
